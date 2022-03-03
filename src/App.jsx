1d0391db1c83256aa62c976e76819bed7850b78f
JuniorCoder <thomas08samuel@gmail.com>
2022-03-03 11:54:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sass/App.sass';
+
+const App = () => {
+  return (
+    <div className="App">
+        <h1>JuniorCoder <span>React Template</span></h1>
+    </div>
+  )
+}
+
+export default App;
